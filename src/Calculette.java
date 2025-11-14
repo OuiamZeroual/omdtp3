76e815473d0847a9cb6155acbcd2643292a6e0fa
Ouiam ZEROUAL <ouiamzeroual@gmail.com>
2025-11-14 09:27:59 +0100

    Ajout fonction multiplication

--- src/Calculette.java
+++ src/Calculette.java
@@ -3,11 +3,12 @@ public class Calculette {
     public static int addition(int a, int b) {
         return a + b;
     }
-
     public static int soustraction(int a, int b) {
         return a - b ;
     }
-
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
     public static void main(String[] args) {
         System.out.println("1 + 3 = " + addition(1,3));
     }
