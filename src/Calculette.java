3b2650b9662a6dcb68d03ea5caeb30ef975ceba9
Ouiam ZEROUAL <ouiamzeroual@gmail.com>
2025-11-14 09:32:55 +0100

    Ajout fonction division

--- src/Calculette.java
+++ src/Calculette.java
@@ -7,8 +7,13 @@ public class Calculette {
     public static int soustraction(int a, int b) {
         return a - b ;
     }
+    //division
+    public static int division(int a, int b) {
+        return a / b;
+    }
 
     public static void main(String[] args) {
         System.out.println("1 + 3 = " + addition(1,3));
     }
+
 }
