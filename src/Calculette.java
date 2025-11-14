b361a253b96b2c5622f5bf13a8ecb2be341c5bc6
Ouiam ZEROUAL <ouiamzeroual@gmail.com>
2025-11-14 09:29:39 +0100

    Amélioration fonction multiplication

--- src/Calculette.java
+++ src/Calculette.java
@@ -1,14 +1,17 @@
 public class Calculette {
-
+    //addition
     public static int addition(int a, int b) {
         return a + b;
     }
+    //soustraction
     public static int soustraction(int a, int b) {
         return a - b ;
     }
+    //multiplication
     public static int multiplication(int a, int b) {
         return a * b;
     }
+    //tests
     public static void main(String[] args) {
         System.out.println("1 + 3 = " + addition(1,3));
     }
